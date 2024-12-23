563979d64ac1cfee4c3ccedc252994a95f264cfd
Valera35981 <valerkamagakyan@gmail.com>
2024-12-23 18:03:27 +0300

    dz3java.java

--- dz3java.java
+++ dz3java.java
@@ -1,69 +1,67 @@
-class dz3 {
-    class Rectangle {
-        private double width;
-        private double height;
+class Rectangle {
+    private double width;
+    private double height;
 
-        public Rectangle(double width, double height) {
-            if (width <= 0 || height <= 0) {
-                throw new RuntimeException("There is no figure with such parameters.");
-            }
-            this.width = width;
-            this.height = height;
+    public Rectangle(double width, double height) {
+        if (width <= 0 || height <= 0) {
+            throw new RuntimeException("There is no figure with such parameters.");
         }
-
-        public double area() {
-            return width * height;
-        }
-
-        public double perimeter() {
-            return 2 * (width + height);
-        }
+        this.width = width;
+        this.height = height;
     }
 
-    class Circle {
-        private double radius;
-
-        public Circle(double radius) {
-            if (radius <= 0) {
-                throw new RuntimeException("There is no figure with such parameters.");
-            }
-            this.radius = radius;
-        }
-
-        public double area() {
-            return Math.PI * radius * radius;
-        }
-
-        public double perimeter() {
-            return 2 * Math.PI * radius;
-        }
+    public double area() {
+        return width * height;
     }
 
-    class Triangle {
-        private double a;
-        private double b;
-        private double c;
-
-        public Triangle(double a, double b, double c) {
-            if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
-                throw new RuntimeException("There is no figure with such parameters.");
-            }
-            this.a = a;
-            this.b = b;
-            this.c = c;
-        }
-
-        public double area() {
-            double s = (a + b + c) / 2;
-            return Math.sqrt(s * (s - a) * (s - b) * (s - c));
-        }
-
-        public double perimeter() {
-            return a + b + c;
-        }
+    public double perimeter() {
+        return 2 * (width + height);
     }
 }
-public class Main {
+
+class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        if (radius <= 0) {
+            throw new RuntimeException("There is no figure with such parameters.");
+        }
+        this.radius = radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Triangle {
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+            throw new RuntimeException("There is no figure with such parameters.");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double area() {
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    public double perimeter() {
+        return a + b + c;
+    }
+}
+public class dz3 {
     public static void main(String[] args) {
         try {
             Rectangle rectangle = new Rectangle(5, 10);
